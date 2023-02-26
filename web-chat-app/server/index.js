becfb9aebe3604cdcd331dd35658181f43f195a6
Daniel Graham <danielgraham@Daniels-MacBook-Pro.local>
2023-02-25 20:17:11 -0500

    create user/message models and user/message routes

---
+++ web-chat-app/server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+
+const app = express();
+
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+// app.use("/api/auth", userRoutes);
+// app.use("/api/messages",messageRoutes);
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("DB Connection successful");
+}).catch((err) => {
+    console.log(err.message);
+});
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`server connected on port:${process.env.PORT}`);
+});
---
+++ web-chat-app/server/routes/messageRoutes.js
@@ -0,0 +1,8 @@
+const {addMessage, getMessages} = require("../controllers/messagesController");
+
+const router = require("express").Router();
+
+router.post("/addmsg",addMessage);
+router.post("/getmsg", getMessages);
+
+module.export = router;
