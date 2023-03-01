11a32c4eba6c264ff64222cca84fd15d9d90d919
Daniel Graham <danielgraham@Daniels-MacBook-Pro.local>
2023-02-28 23:46:56 -0500

    fix bugs with logout functionality, start main chat layout

---
+++ web-chat-app/client/src/components/Friends.jsx
@@ -0,0 +1,19 @@
+
+
+export default function Friends(){
+    return (
+        <div className="friends-container">
+            <div className="friend">Sample Friend</div>
+            <div className="friend">Sample Friend</div>
+            <div className="friend">Sample Friend</div>
+            <div className="friend">Sample Friend</div>
+            <div className="friend">Sample Friend</div>
+            <div className="friend">Sample Friend</div>
+            <div className="friend">Sample Friend</div>
+            <div className="friend">Sample Friend</div>
+            <div className="friend">Sample Friend</div>
+            <div className="friend">Sample Friend</div>
+            <div className="friend">Sample Friend</div>
+        </div>
+    )
+}
--- web-chat-app/client/src/pages/Chat.jsx
+++ web-chat-app/client/src/pages/Chat.jsx
@@ -1,7 +1,42 @@
+import Friends from '../components/Friends';
+import { useState, useEffect } from 'react';
+import {useNavigate} from 'react-router-dom';
 
+export default function Chat(){ 
+    const navigate = useNavigate();
+    const [curUser, setCurUser] = useState({});
 
-export default function Chat(){
+    async function fetchUser(){
+        if (localStorage.getItem('react-chat-user') === null){
+            navigate("/login");
+        }else{
+            setCurUser(await JSON.parse(localStorage.getItem('react-chat-user')));
+        }
+    };
+
+    useEffect(() => {
+        fetchUser();
+    }, [curUser]);
+
+    function logout(){
+        localStorage.removeItem('react-chat-user');
+        setCurUser({});
+    }
+
     return (
-        <div>THIS IS THE CHAT SCREEN</div>
+        <div className='chat-container'>
+            <div className='chat-navbar'>
+                <div className="chat-header">Hello, {curUser.username}</div>
+                <button className="logout-btn" onClick={logout}>Logout</button>
+            </div>
+            
+            <div className='chat-main'>
+                <Friends />
+                <div className='chat-area'>
+                    This is the main chat window
+                </div>
+            </div>
+            
+        </div>
     )
 }
